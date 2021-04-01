fb0dc48b3467f75b4ad00a1a1557968805add929
Suraj Mishra <75450225+mishrasur7@users.noreply.github.com>
2021-04-01 20:39:01 +0300

    Update player_code.js

--- Step_0_Only_HTML_page/player_code.js
+++ Step_0_Only_HTML_page/player_code.js
@@ -46,4 +46,66 @@ function findWinner() {
 // ----------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------
+//Solving it without using ready-made functions 
+var player = []; 
+var scores = [];
+var i = 0; 
+//------------------------------------------------------------------------------------------------
+function addPlayer() {
+    var name = document.getElementById("txtName").value; 
+    var score = Number(document.getElementById("txtScore").value);
+    if (name === "" || score === "") {
+        alert("Please enter name and score."); 
+    } 
+    else {
+        player.push(name); 
+        scores.push(score); 
+        document.getElementById("txtName").value = ""; 
+        document.getElementById("txtScore").value = "";
+    }
+   
+}
+
+//------------------------------------------------------------------------------------------------
+function listPlayer() {
+    var outPutText = ""; 
+    for(var i = 0; i < player.length; i++) {
+        outPutText += player[i] + "'s score is " + scores[i] + "<br />"; 
+    }
+    document.getElementById("pOutput").innerHTML = outPutText; 
+}
+//This is not ready yet but still working on it. ------------------------------------------------------------------------------------------------
+function playerSearch() {
+    var nameInput = document.getElementById("text").value; 
+    var outPutText; 
+    for(var i = 0; i < player.length; i++) {
+        if(nameInput === player[i]) {
+            outPutText = nameInput + "'s score is " + scores[i]; 
+        }
+        else {
+            outPutText = "Sorry we did not find this player!"; 
+        }
+    }
+    document.getElementById("pOutput2").innerHTML = outPutText; 
+}
+//------------------------------------------------------------------------------------------------
+function findWinner() {
+    if(scores.length >= 1) {
+        var maxSoFar = scores[0]; 
+        for(var i = 1; i < scores.length; i++) {
+            if(maxSoFar < scores[i]) {
+                maxSoFar = scores[i]; 
+            }
+        }
+        var winner = "The winners are: "; 
+        for(var i = 0; i < scores.length; i++) {
+            if(maxSoFar === scores[i]) {
+                winner += player[i] + scores[i] + " points, "; 
+            }
+        }
+        document.getElementById("pOutput3").innerHTML = winner; 
+    } else {
+        document.getElementById("pOutput3").innerHTML = "Error! Empty array."; 
+    }
+}
 
